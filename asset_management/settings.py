a777c74316e23669612df44acf9ca4941c307d93
Foysal Ahmed <mfoysal314@gmail.com>
2024-03-02 01:27:10 +0600

    authentication added with djoser and simplejwt

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -1,20 +1,14 @@
 from pathlib import Path
+from datetime import timedelta
 from decouple import config
-
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
 SECRET_KEY = config('SECRET_KEY')
-
 
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #External apps
     'rest_framework',
+    'djoser',
+    #Internal
     'assetmanager',
     'authentication',
 ]
@@ -59,9 +55,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'asset_management.wsgi.application'
 
-
-
-
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
@@ -74,7 +67,7 @@ DATABASES = {
 	}
 }
 
-
+AUTH_USER_MODEL = 'authentication.Company'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -91,10 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -103,35 +92,23 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#External dictionary
-
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',  # Default JSON renderer
-        'rest_framework.renderers.BrowsableAPIRenderer',  # Optional: for browsable API
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',  # Default JSON parser
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',  # Session-based authentication
-        # Add other authentication classes as needed (e.g., TokenAuthentication, JWTAuthentication)
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',  # Requires authentication for all views
-        # Add other permission classes as needed
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days = 1),
+}
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'authentication.serializers.UserCreateSerializer'
+    }
 }
--- authentication/models.py
+++ authentication/models.py
@@ -1,11 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, name,description, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
 
+        user = self.model(
+            email=self.normalize_email(email),
+            name = name,
+            description = description,
+        )
 
-class Company(models.Model):
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email,name,description, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+            description=description
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class Company(AbstractBaseUser):
     name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
     description = models.TextField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name','description']
+
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
 
     def __str__(self) -> str:
         return f"{self.name}"    
